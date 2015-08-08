8c0325bd5e327f686e2c2d5db3845933640d7c01
monsteronfire <monsteronfire@gmail.com>
2015-08-08 12:29:18 +0800

    adds adaptor directory

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,1 +1,12 @@
 import DS from 'ember-data';
+
+var Todo = DS.Model.extend({
+	title: DS.attr('string'),
+	isCompleted: DS.attr('boolean')
+});
+
+Todo.reopenClass({
+	
+});
+
+export default Todo;
